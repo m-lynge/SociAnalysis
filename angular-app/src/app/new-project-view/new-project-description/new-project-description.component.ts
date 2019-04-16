f9075bfd78f014ee8adedab6cb102c7ee6fc32c0
Unknown <guitartard@hotmail.com>
2019-04-16 10:37:08 +0200

    Project description page functionality should be up and running

--- angular-app/src/app/new-project-view/new-project-description/new-project-description.component.ts
+++ angular-app/src/app/new-project-view/new-project-description/new-project-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewProjectService } from 'src/app/new-project.service';
 
 @Component({
   selector: 'app-new-project-description',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-project-description.component.css']
 })
 export class NewProjectDescriptionComponent implements OnInit {
+//Variables used to store the input
+title: string ='';
+description: string ='';
 
-  constructor() { }
+  constructor(private newService: NewProjectService) { }
 
   ngOnInit() {
   }
+
+getTitle(){
+  this.newService.setName(this.title);
+  //console.log(this.title);
+}
+
+getDesc(){
+  this.newService.setDescription(this.description);
+  //console.log(this.description);
+}
 
 }
