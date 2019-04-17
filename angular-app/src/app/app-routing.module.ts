b430fe1b51092ddd7044dd0a668d1756f9bf4ab4
Unknown <nolse16@student.aau.dk>
2019-04-17 13:10:14 +0200

    Added projekt route and fixed nav margin

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { LoginViewComponent } from './login-view/login-view.component';
 import { LoadingViewComponent } from './loading-view/loading-view.component';
 import { HomeViewComponent } from './home-view/home-view.component';
 import { NewProjectViewComponent } from './new-project-view/new-project-view.component';
+import { ProjectViewComponent } from './project-view/project-view.component';
 
-const routes: Routes = [  {path: '', component: LoginViewComponent},
-{path: 'loading', component: LoadingViewComponent},
- {path: 'home', component: HomeViewComponent}, {path: 'opretprojekt', component: NewProjectViewComponent}];
+
+const routes: Routes = [{ path: '', component: LoginViewComponent },
+{ path: 'loading', component: LoadingViewComponent },
+{ path: 'home', component: HomeViewComponent },
+{ path: 'opretprojekt', component: NewProjectViewComponent },
+{ path: 'projekt', component: ProjectViewComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
