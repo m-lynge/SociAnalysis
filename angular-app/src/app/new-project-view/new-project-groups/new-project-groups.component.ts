86bb204e1faf2a6c383dffdfe8ca71dc87e4c329
Unknown <guitartard@hotmail.com>
2019-04-18 14:44:29 +0200

    Can now navigate to project overview. Also added some CSS
    Still looks shit though.

--- angular-app/src/app/new-project-view/new-project-groups/new-project-groups.component.ts
+++ angular-app/src/app/new-project-view/new-project-groups/new-project-groups.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, NgModule } from '@angular/core';
-
-
+import {
+  Component,
+  OnInit,
+  NgModule,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { MatChipInputEvent } from "@angular/material/chips";
+import { COMMA, ENTER, SPACE } from "@angular/cdk/keycodes";
+import { Group } from "../../Group";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-project-groups',
-  templateUrl: './new-project-groups.component.html',
-  styleUrls: ['./new-project-groups.component.css']
+  selector: "app-new-project-groups",
+  templateUrl: "./new-project-groups.component.html",
+  styleUrls: ["./new-project-groups.component.css"]
 })
-
-
 export class NewProjectGroupsComponent implements OnInit {
+  @Output() show: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showNext(): void {
+    this.show.emit(2);
   }
-
 }
