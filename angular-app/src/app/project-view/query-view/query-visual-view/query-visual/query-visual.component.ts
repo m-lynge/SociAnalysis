c962626d02332f7fa737809601eb94c631204e38
LynzZen <mathias.s.lynge@gmail.com>
2019-04-22 19:53:57 +0200

    Done some visualisations

--- angular-app/src/app/project-view/query-view/query-visual-view/query-visual/query-visual.component.ts
+++ angular-app/src/app/project-view/query-view/query-visual-view/query-visual/query-visual.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+import * as d3Force from 'd3-force';
+
+import { WORDS } from './words';
+import { style } from '@angular/animations';
+import { anchorDef } from '@angular/core/src/view';
+import { NavigationService } from 'src/app/navigation.service';
 
 @Component({
   selector: 'app-query-visual',
@@ -7,9 +18,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryVisualComponent implements OnInit {
 
-  constructor() { }
+  height = 500;
+  width = 500;
+  private svg: any;
+  private circles: any;
+  private chart: any;
 
-  ngOnInit() {
+  private simulation = d3Force.forceSimulation()
+  .force('x', d3Force.forceX(this.width / 2).strength(0.5))
+  .force('y', d3Force.forceY(this.height / 2).strength(0.5));
+
+  constructor(private navigationservice: NavigationService) {
+    navigationservice.setNavi = false;
   }
+
+  ngOnInit(): void {
+    this.InitSVG();
+    this.DrawCirles();
+  }
+
+
+
+  private InitSVG() {
+    this.svg = d3.select('svg')
+      .attr('height', this.height)
+      .attr('width', this.width)
+      .append('g')
+      .attr('transform', 'translate(0,0)');
+  }
+
+  private DrawCirles() {
+    this.circles = this.svg.selectAll('.WORDS')
+      .data(WORDS)
+      .enter()
+      .append('circle')
+      .attr('class', 'words')
+      .attr('r', 10)
+      .attr('fill', 'lightblue');
+    this.simulation.nodes(WORDS)
+      .on('tick', this.ticked());
+
+
+  }
+  private ticked() {
+    console.log("run ticked");
+    this.circles
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y);
+  }
+
 
 }
---
+++ angular-app/src/app/project-view/query-view/query-visual-view/query-visual/words.ts
@@ -0,0 +1,5 @@
+export const WORDS: any[] = [
+    {word: 'Pølse', amount: 10},
+    {word: 'Trump', amount: 20},
+    {word: 'Mor', amount: 20},
+];
