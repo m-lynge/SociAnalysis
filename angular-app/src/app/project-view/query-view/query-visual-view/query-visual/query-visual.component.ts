f3bf7ef8930a409a66b8ca5a54fdf3721fdc9b93
LynzZen <mathias.s.lynge@gmail.com>
2019-04-24 10:14:29 +0200

    Vis. done

--- angular-app/src/app/project-view/query-view/query-visual-view/query-visual/query-visual.component.ts
+++ angular-app/src/app/project-view/query-view/query-visual-view/query-visual/query-visual.component.ts
@@ -25,17 +25,15 @@ export class QueryVisualComponent implements OnInit, AfterContentInit {
 
 
   // Variable used for the word freq counter:
-// tslint:disable-next-line: max-line-length
-  private rawText = 'own considerable abilities  and nervously exaggerated them in his self  conceit  He knew he would play a prominant part of some sort  but Alyosha  who was attached to him  was distressed to see that his friend Rakitin was dishonorble  and quite unconscios of being so himself  considering  on the contrary  that because he would not steal moneey left on the table he was a man of the highest integrity  Neither Alyosha nor anyone else could have infleunced him in that Rakitin  of course  was a person of tooo little consecuense to be invited to the dinner  to which Father Iosif  Father Paissy  and one othr monk were the only inmates of the monastery invited  They were alraedy waiting when Miusov  Kalganov  and Ivan arrived  The other guest  Maximov  stood a little aside  waiting also  The Father Superior stepped into the middle of the room to receive his guests  He was a tall  thin  but still vigorous old man  with black hair streakd with grey  and a long  grave  ascetic face  He bowed to his guests in silence  But this time they approaced to receive his blessing  Miusov even tried to kiss his hand  but the Father Superior drew it back in time to aboid the salute  But Ivan and Kalganov went through the ceremony in the most simple  hearted and complete manner  kissing his hand as peesants do';
-  private textOccurrences;
-  private statsArray;
-
-
-
-  private simulation: any;
+  // tslint:disable-next-line: max-line-length
+  private rawText = 'pappa pappa pappa pappa pappa rasmus rasmus rasmus rasmus rasmus cuense to be invited to the dinner r Paisnly inmates of the monastery invited  They were alraedy waiting when Miusov  Kalganov  and Ivan arrived  The other guest  Maximov  stood a little aside  waiting also  The Father Superior stepped into the middle of the room to receive his guests  He was a tall  thin  but still vigorous old man  with black hair streakd with grey  and a long  grave  ascetic face  He bowed to his guests in silence  But this time they approaced to receive his blessing  Miusov even tried to kiss his hand  but the Father Superior drew it back in time to aboid the salute  But Ivan and Kalganov went through the ceremony in the most simple  hearted and complete manner  kissing his hand as peesants do';
+  private textOccurrences: any;
+  private statsArray: any;
+  private sortedStatsArray;
+  private simulation;
 
   constructor(private navigationservice: NavigationService) {
-    navigationservice.setNavi = false;
+    this.navigationservice.setNavi = true;
     this.height = 500;
     this.width = 500;
 
@@ -54,35 +52,32 @@ export class QueryVisualComponent implements OnInit, AfterContentInit {
     this.svg = d3.select('svg')
       .attr('height', this.height)
       .attr('width', this.width)
+      .style('text-anchor', 'middle')
       .append('g')
       .attr('transform', 'translate(250,250)');
 
     // calculate max amount of words to make the domain correct:
-    this.max = 0;
-    for (const i in this.statsArray) {
-      if (this.statsArray[i].number > this.max) {
-        this.max = this.statsArray[i].number;
-      }
-    }
+    this.max = this.sortedStatsArray[0].number;
+    console.log(this.max);
 
     // mapping the domain to a range for calculating the correct radius for the circles
     this.ratioScale = d3Scale
       .scaleSqrt()
       .domain([1, this.max])
-      .range([30, 150]);
+      .range([30, 100]);
   }
 
   private DrawCirles() {
 
     this.circles = this.svg.selectAll('.words')
-      .data(this.statsArray)
+      .data(this.sortedStatsArray)
       .enter()
       .append('circle')
       .attr('class', 'words')
       .attr('r', (d) => this.ratioScale(d.number))
-      .attr('fill', (d) => d.number > 4 ? 'red' : 'green');
+      .attr('fill', (d) => d = 'hsl(' + Math.random() * 360 + ',50%,50%)');
     this.text = this.svg.selectAll('.text')
-      .data(this.statsArray)
+      .data(this.sortedStatsArray)
       .enter()
       .append('text')
       .text((d) => d.word);
@@ -104,7 +99,7 @@ export class QueryVisualComponent implements OnInit, AfterContentInit {
       .force('x', d3Force.forceX(0).strength(0.05))
       .force('y', d3Force.forceY(0).strength(0.05))
       .force('collide', d3Force.forceCollide((d) => this.ratioScale(d.number)))
-      .nodes(this.statsArray)
+      .nodes(this.sortedStatsArray)
       .on('tick', () => this.ticked());
   }
 
@@ -113,6 +108,11 @@ export class QueryVisualComponent implements OnInit, AfterContentInit {
     this.statsArray = Object.keys(this.textOccurrences.stats).map(key => {
       return { word: key, number: this.textOccurrences.stats[key] };
     });
-    console.log(this.statsArray);
+
+    this.sortedStatsArray = this.statsArray.sort((a, b) => (a.number > b.number) ? 1 : ((b.number > a.number) ? -1 : 0));
+    this.sortedStatsArray.reverse();
+
+    this.sortedStatsArray = this.sortedStatsArray.slice(0, 10);
+    console.log(this.sortedStatsArray);
   }
 }
