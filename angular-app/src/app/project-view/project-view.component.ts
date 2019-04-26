52c6f07ee1f6c03cf22f441166008320f4a51d02
Heine2 <basdreng1902@gmail.com>
2019-04-26 12:14:19 +0200

    Route back to home, if not logged in!

--- angular-app/src/app/project-view/project-view.component.ts
+++ angular-app/src/app/project-view/project-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {DirectoryService} from "../directory.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-project-view',
-  templateUrl: './project-view.component.html',
-  styleUrls: ['./project-view.component.css']
+    selector: 'app-project-view',
+    templateUrl: './project-view.component.html',
+    styleUrls: ['./project-view.component.css']
 })
-export class ProjectViewComponent implements OnInit {
+export class ProjectViewComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+    constructor(private directoryservice: DirectoryService, private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        if (!this.directoryservice.selectedUser) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngAfterViewInit(): void {
+
+
+    }
 
 }
